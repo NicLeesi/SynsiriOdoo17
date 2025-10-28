6f34b5b7a49f8fd6fabfa657dd90269eaa74ce61
Nic Leesirikul <workflownic@gmail.com>
2025-10-28 13:25:01 +0700

    update logo app in custom_pwa_brand 1

--- custom/dev/custom_favicon/__manifest__.py
+++ custom/dev/custom_favicon/__manifest__.py
@@ -1,7 +1,7 @@
 {
   "name": "Custom Favicon (Login + Website) – Odoo 17",
   "version": "17.0.1.1",
-  "depends": ["web", "website"],  # website optional, but safe if installed
+  "depends": ["web"],  # website optional, but safe if installed
   "data": ["views/favicon.xml"],
   "license": "LGPL-3"
 }
